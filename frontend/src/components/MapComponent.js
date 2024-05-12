a36966668603e2bd76e623e61660a3d5a35d35a1
Samyak Jain <jainsamyak107@gmail.com>
2024-05-12 00:08:50 -0400

    feat: initial commit

---
+++ frontend/src/components/MapComponent.js
@@ -0,0 +1,8 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+
+const center = [6.5244, 3.3792];
+const zoom = 13;
+
+export default function MapComponent() {
+  return <div>MapComponent</div>;
+}
