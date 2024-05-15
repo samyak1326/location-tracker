42432f287a3c1c8b0a67b6cacc1421f3df00d7f8
Samyak Jain <jainsamyak107@gmail.com>
2024-05-15 15:45:23 -0400

    feat: edit location name

--- backend/controllers/location.controller.js
+++ backend/controllers/location.controller.js
@@ -1,53 +1,42 @@
 const parser = require("papaparse");
 const User = require("../model/User.model");
 
-exports.addLocation = async (req, res) => {
-  try {
-    // extract location details from the user
-    const { name, latitude, longitude } = req.body;
-    // extract the user's ID
-    const { _id } = req.user;
-
-    // update user with new location details
-    const updatedUser = await User.findByIdAndUpdate(
-      _id,
-      {
-        $push: {
-          locations: {
-            name,
-            latitude,
-            longitude,
+exports.editLocation = async (req, res) => {
+    try {
+      // extract the user's ID
+      const { _id } = req.user;
+      // extract location details from the request body
+      const { id: locationId, name: newLocationName } = req.body;
+  
+      // update user with new location details
+      const updatedUser = await User.findOneAndUpdate(
+        { _id, "locations._id": locationId },
+        {
+          $set: {
+            "locations.$.name": newLocationName,
           },
         },
-        $set: {
-          currentLocation: {
-            name,
-            latitude,
-            longitude,
-          },
-        },
-      },
-      { new: true, useFindAndModify: false },
-    );
-
-    // if a user is not found
-    if (!updatedUser) {
-      return res.status(404).json({
-        message: "User not found",
+        { new: true },
+      );
+  
+      // if the location is not found
+      if (!updatedUser) {
+        return res.status(404).json({
+          message: "Location not found",
+          data: null,
+        });
+      }
+  
+      res.status(200).json({
+        message: "Location updated successfully",
+        data: updatedUser,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
         data: null,
       });
     }
-
-    res.status(200).json({
-      message: "Location added successfully!",
-      data: updatedUser,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-      data: null,
-    });
-  }
 };
 
 exports.getLocationCsvData = async (req, res) => {};
