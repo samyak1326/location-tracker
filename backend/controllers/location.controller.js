f49772f5fdcb746524d03d3e8a4ad29a36b09274
Samyak Jain <jainsamyak107@gmail.com>
2024-05-15 15:59:14 -0400

    feat: user delete location

--- backend/controllers/location.controller.js
+++ backend/controllers/location.controller.js
@@ -41,8 +41,43 @@ exports.editLocation = async (req, res) => {
 
 exports.getLocationCsvData = async (req, res) => {};
 
-exports.deleteLocation = async (req, res) => {};
+exports.deleteLocation = async (req, res) => {
+    try {
+      // extract the user's ID
+      const { _id } = req.user;
+      // extract location details from the request body
+      const { id: locationId } = req.body;
+  
+      // update user with new location details
+      const location = await User.findOneAndUpdate(
+        { _id, "locations._id": locationId },
+        {
+          $pull: {
+            locations: { _id: locationId },
+          },
+        },
+        { new: true },
+      );
+  
+      // if the location is not found
+      if (!location) {
+        return res.status(404).json({
+          message: "Location not found",
+          data: null,
+        });
+      }
+  
+      res.status(200).json({
+        message: "Location deleted successfully!",
+        data: location,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+        data: null,
+      });
+    }
+};
 
-exports.editLocation = async (req, res) => {};
 
 exports.getUser = async (req, res) => {};
--- backend/routes/location.router.js
+++ backend/routes/location.router.js
@@ -10,5 +10,8 @@ const { authMiddleware } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router.post("/add", authMiddleware, addLocation);
+router.put("/edit", authMiddleware, editLocation);
+router.delete("/delete", authMiddleware, deleteLocation);
+// router.put("update/", authMiddleware)
 
 module.exports = router;
