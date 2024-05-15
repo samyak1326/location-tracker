7484e8d5db13cb551f3bbd56e1dd849598820e77
Samyak Jain <jainsamyak107@gmail.com>
2024-05-15 18:23:23 -0400

    feat: share location as link frontend

--- frontend/src/pages/UserLocation.js
+++ frontend/src/pages/UserLocation.js
@@ -1,20 +1,73 @@
-import { useEffect, useState, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import {
-  MapContainer, Marker, Popup, TileLayer,
-} from 'react-leaflet';
-import Leaflet from 'leaflet';
-import useAuth from '../hooks/useAuth';
-import serverURL from '../utils/urls';
-import Loader from '../components/Loader';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import Leaflet from "leaflet";
+import useAuth from "../hooks/useAuth";
+import serverURL from "../utils/urls";
+import Loader from "../components/Loader";
 
 export default function UserLocation() {
+  // get username from the request parameters
+  const { username } = useParams();
+
+  // create the state variables
+  const [userDetails, setUserDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // invoke the custom authentication hook
+  useAuth();
+
+  useEffect(() => {
+    // create the getUser function
+    const getUser = async () => {
+      try {
+        // get result data from request
+        const {
+          data: { data },
+        } = await axios.get(`${serverURL}/locations/user/${username}`);
+
+        // update states
+        setUserDetails(data);
+        setLoading(false);
+      } catch (error) {
+        const responseError = error?.response?.data?.message;
+        toast.error(responseError || error.message);
+        setLoading(false);
+      }
+    };
+
+    // invoke the getUser() function
+    getUser();
+  }, [username]);
 
   return (
     <div className="p-3 h-screen lg:p-10 py-10 dark:text-white border mt-10">
-      User Location Page
+      {loading ? (
+        <Loader />
+      ) : userDetails ? (
+        <div className="h-screen w-full  flex flex-col ">
+          <div className="h-1/6">
+            <p className="my-3">
+              This user has{" "}
+              <span className="text-green-500">
+                {userDetails?.friends?.length}
+              </span>{" "}
+              friends
+            </p>
+            <p>
+              And has visited{" "}
+              <span className="text-green-500">
+                {userDetails?.locations?.length}
+              </span>{" "}
+              place(s).
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div>User details not available</div>
+      )}
     </div>
   );
 }
